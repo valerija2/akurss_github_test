8ef90240362a6e010dd00b86c0dbb353e945c5f6
valerija2 <hocus2008@gmail.com>
2018-04-21 19:32:58 +0300

    changes2

---
+++ src/lv/akurss/github/test/Main.java
@@ -0,0 +1,10 @@
+package lv.akurss.github.test;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person p = new Person ("John");
+
+        System.out.println(p.getName());
+    }
+}
